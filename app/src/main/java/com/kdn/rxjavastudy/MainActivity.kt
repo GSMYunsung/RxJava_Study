ab77d47b61a9626914370b1b0e01bf6eae759920
choiyunsung <12345ggh81@gmail.com>
2022-03-28 20:16:17 +0900

    [CREATE] 안드로이드 Rxjava 공부

---
+++ app/src/main/java/com/kdn/rxjavastudy/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.kdn.rxjavastudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //람다식으로 구현하는 방법
+
+        // 반환값으로 반환된 Observable 의 해당 Observer 가 해당 구독에 대한 알림을 반환해주어 결과를 호출할 수 있다.
+
+        rangeOperator().subscribe({
+            Log.d(TAG,"onSubscribe : $it")
+        },
+        {
+            Log.d(TAG,"onError $it")
+        },
+        {
+            Log.d(TAG,"onComplete")
+        })
+
+    }
+
+}
